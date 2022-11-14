54c8dcd599b9cf92b142e3c39f4c6b1fdbd2ac9c
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-11-14 16:32:05 +0100

    Tag 1: Grundlagen, Schleifen, Bedingungen, Enums

--- Arrays_Bedingungen/Program.cs
+++ Arrays_Bedingungen/Program.cs
@@ -1,54 +1,93 @@
-﻿using System.Reflection.Metadata;
-
-namespace Arrays_Bedingungen
+﻿namespace Array_Bedingungen
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            int[] zahlen = { 2, 4, 78, -123, 0, -8, 11111 };
+            #region Arrays
+
+            //ARRAYS
+            ///Arrays sind Collections, welche mehrere Variablen eines Datentyps speichern können. Die Größe des Arrays muss bei der
+            ///Initialisierung entweder durch eine Zahl oder durch eine bestimmte Anzahl von spezifischen Elementen definiert werden.
+            int[] zahlen = { 2, 4, 78, -123, -8, 0, 11111 };
+            //Der Zugiff auf einzelne Array-Positionen erfolgt durch einen Nullbasierten Index
             Console.WriteLine(zahlen[2]);
             zahlen[2] = 1234;
             Console.WriteLine(zahlen[2]);
 
+            //Array-Deklaration ohne direkte Initialisierung der Positionen (Größe muss angegeben werden)
             string[] worte = new string[5];
 
+            //Neuzuweisung eines neuen Arrays zu einer existierenden Arrayvariablen 
             zahlen = new int[] { 2, 4 };
 
-            Console.WriteLine(zahlen.Contains(2));
-            Console.WriteLine(zahlen.Contains(45));
+            //Verwendung der Contains-Funktion eines Arrays (überprüft auf das Vorhandensein eines Elements)
+            Console.WriteLine(zahlen.Contains(-123));
+            Console.WriteLine(zahlen.Contains(555));
 
+            //Ausgabe der Länge (Anzahl der Elemente) des Arrays
             Console.WriteLine(zahlen.Length);
 
-
+            //String als Char-Array
             string beispiel = "Hallo";
             Console.WriteLine(beispiel[2]);
 
+            //Mehrdimensionales Array
+            int[,] zweiDimArray = new int[3, 5];
+            zweiDimArray[0, 0] = 1;
 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    zweiDimArray[i, j] = i * j;
+                }
+            }
+            Console.WriteLine(zweiDimArray[2, 3]);
+            #endregion
 
+            #region Bedingungen (If/Else)
+
+            //Deklaration und Initialisierung von Beispiel-Variablen
             int a = 23;
             int b = 23;
 
-            if(a < b)
+            //BEDINGUNGEN (IF) prüfen logische Aussagen (z.B. Vergeliche oder Funktinen mit bool-Rückgabe) und führen den folgenden Block bei einem TRUE aus
+            if (a == 20)
             {
-                Console.WriteLine($"{a} ist kleiner als {b}");
+                Console.WriteLine("A ist 20");
             }
-            else if(a > b)
+
+            //IF-ELSEIF-ELSE-Block
+            ///Das Programm wird den ersten Block ausführen, bei welchem er auf eine wahre Bedingung trifft und dann am Ende des Blocks mit
+            ///dem Code weiter machen
+            if (a < b)
             {
-               Console.WriteLine("A ist größer als B");
+                Console.WriteLine("A ist kleiner als B");
             }
+            //Es kann beliebig viele ELSE-IF-Blöcke geben
+            else if (a > b)
+            {
+                Console.WriteLine("A ist größer als B");
+            }
+            //Wenn keine der Bedingungen wahr ist, wird der (optionale) ELSE-Block ausgeführt
             else
                 Console.WriteLine("A ist gleich B");
 
+            //Kurznotation:
+            //(Bedingung) ? TrueAusgabe : FalseAusgabe
+            string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
 
+            //Bsp eines String-Vergleichs
             string name1 = "Hans";
             string name2 = "Hans";
 
-            if(name1.Equals(name2))
+            if (name1.Equals(name2))
+            {
                 Console.WriteLine("Gleich");
+            }
+            #endregion
 
-
-            string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
         }
     }
 }
---
+++ Schleifen_Enums/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace Schleifen_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Schleifen
+
+            int a = 0;
+            int b = 10;
+
+            //WHILE-Schleife
+            ///Die WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Ist die Bedingung von vornherein unwahr, dann wird die Schleife
+            ///übersprungen
+            while (a < b)
+            {
+                Console.WriteLine("A ist kleiner B");
+                a++;
+
+                //Mit der BREAK-Anweisung wird die Schleife verlassen und der Code wird fortgesetzt.
+                if (a == 5)
+                    break;
+            }
+            Console.WriteLine("Schleifenende");
+
+            a = -45;
+            //DO-WHILE-Schleife
+            ///Auch die DO-WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Allerdings wird die Bedingung erst am Schleifen_
+            ///ende geprüft, weshalb die Schleife mindestens einmal durchläuft.
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+
+                //Der CONTINUE-Befehl beendet den aktuellen Schleifendurchlauf und lässt erneut die Bedingung prüfen. Ist die Bedingung wahr
+                ///beginnt ein neuer Durchlauf
+                continue;
+
+            } while (a > 0);
+
+
+            //FOR-Schleife
+            ///Der FOR-Schleife wird ein Laufindex (i) sowie eine Bedingung und eine Anweisung übergeben. Am Ende jedes Durchlaufes wird die
+            ///Anweisung ausgeführt. Wenn die Bedingung nicht (mehr) wahr ist, wird kein (weiterer) Schleifendurchlauf begonnen.
+            for (int i = 0; i < 10; i += 2)
+            {
+                Console.WriteLine(i);
+
+                i++;
+            }
+
+            int[] zahlen = { 2, 3, 5, 4 };
+            //Iteration über ein Array mittels For-Schleife
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine(zahlen[i]);
+            }
+
+            //FOREACH-Schleifen können über Collections laufen und sprechen dabei jedes Element genau einmal an
+            foreach (int item in zahlen)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+
+            #region Enums
+
+            //ENUMs sind spezialisierte selbst-definierte Datentypen mit festgelegten möglichen Zuständen.
+            ///Dabei ist jeder Zustand an einen Integer-Wert gekoppelt, wodurch eine explizite Umwandlung (Cast) möglich ist. (vgl. Datentyp-Definition unten)
+
+            //Deklaration und Initialisierung einer Variablen vom Enumerator-Typ
+            Wochentag heutigerTag = Wochentag.Dienstag;
+            Console.WriteLine($"Heute ist also {heutigerTag}.");
+
+            //For-Schleife über die möglichen Zustande des Enumerators
+            Console.WriteLine("Welcher Wochentag ist dein Lieblingstag?");
+            for (int i = 1; i < Enum.GetValues(typeof(Wochentag)).Length; i++)
+            {
+                Console.WriteLine($"{i}: {(Wochentag)i}");
+            }
+            //Speichern einer Benutzereingabe (Int) als Enumerator
+            //Cast: Int -> Wochentag
+            heutigerTag = (Wochentag)int.Parse(Console.ReadLine());
+            Console.WriteLine($"Dein Lieblingstag ist also {heutigerTag}.");
+
+            //Parsing eines Strings in den Enumzustand
+            heutigerTag = (Wochentag)Enum.Parse(typeof(Wochentag), "Freitag");
+            Console.WriteLine($"Dein Lieblingstag ist also {heutigerTag}.");
+
+            //SWITCHs sind eine verkürzte Schreibweise für IF-ELSE-Blöcke. Mögliche Zustände der übergebenen Variablen werden 
+            //in den CASES definiert
+            switch (heutigerTag)
+            {
+                case Wochentag.Montag:
+                    Console.WriteLine("Wochenstart");
+                    //Jeder speziell definierte CASE muss mit einer BREAK-Anweisung beendet werden
+                    break;
+                //Wenn in einem CASE keine Anweisungen geschrieben stehen kann auf den BREAK-Befehl verzichtet werden. In diesem Fall
+                //springt das Programm in diesem CASE zum Nachfolgenden
+                case Wochentag.Dienstag:
+                case Wochentag.Mittwoch:
+                case Wochentag.Donnerstag:
+                    Console.WriteLine("Normaler Wochentag");
+                    break;
+                case Wochentag.Freitag:
+                case Wochentag.Samstag:
+                case Wochentag.Sonntag:
+                    Console.WriteLine("Wochenende");
+                    break;
+                //Wenn die übergebene Variable keinen der vordefinierten Zustände erreicht, wird der DEFAULT-Fall ausgeführt
+                default:
+                    Console.WriteLine("Fehlerhafte Eingabe");
+                    break;
+            }
+
+            //Mittels des WHEN-Stichworts kann auf Eigenschaften des betrachteten Objekts näher eingegangen werden
+            int zahl = -45;
+            switch (zahl)
+            {
+                case 5:
+                    Console.WriteLine("a ist 5");
+                    break;
+                //zahl wird in z eingelegt (zu überprüfende Variable wird für Bedingungsprüfung vorbereitet)
+                //und mittels when auf eine Eigenschaft geprüft
+                case int z when z < 0:
+                    Console.WriteLine("a < 0");
+                    break;
+                default:
+                    break;
+            }
+
+            //Bsp für von C# verwendetem Enum (hier ConsoleKey = Abbildung der Tastaturtasten)
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                Console.WriteLine("Du hast Enter gedrückt");
+            #endregion
+
+        }
+    }
+
+    //Enum-Definition (vgl. Verwendung oben)
+    enum Wochentag { Montag = 1, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag }
+
+}
